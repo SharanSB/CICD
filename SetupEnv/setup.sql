9714d6cb9d7f6c203cff1b16f8cf9b4ed144b48b
Sharan SB <91715177+SharanSB@users.noreply.github.com>
2025-08-19 15:13:18 +0530

    Update setup.sql

--- SetupEnv/setup.sql
+++ SetupEnv/setup.sql
@@ -1,11 +1,1 @@
--- Set the role to SYSADMIN or your deployment role
-USE ROLE CICD_ROLE;
-
--- Set the warehouse to the one used for deployments
-USE WAREHOUSE CICD_WH;
-
--- Set the database where your objects will be created
-USE DATABASE CICD_PROD_DB;
-
--- Set the schema where your tables and views will reside
-USE SCHEMA PUBLIC;
+-- gfmtehguyht
